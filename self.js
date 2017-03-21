1d6ea49a714c05a1584a80091dcc5f4144adceea
Licshee <Licshee@users.noreply.github.com>
2017-03-22 03:04:50 +0800

    Add files via upload

--- self.js
+++ self.js
@@ -9,18 +9,33 @@ if(typeof that === "object")
 else if(that == null && this.navigator)
   $.call(this);
 return $;
-}(function self(){
+}(function self(ctxt){
   var me = this;
   me.self||(me.self=me);
+
+  if(ctxt == null || typeof ctxt !== "object")
+    ctxt = { String: String, Object: Object };
+  var object = ctxt.Object;
+  var object$prototype = object.prototype;
+  var object$toString = object$prototype.toString;
+  var string = ctxt.String;
+  function isString($){ return object$toString.call($) === "[object String]"; }
+  function any2string($){ return isString($) ? $ : string($); }
+
+  this.isString = isString;
+  this.any2string = any2string;
+
   var wshOut = me.WScript && function($){ WScript.Echo($); };
   this.log = wshOut || function($){ console.log($); };
   this.msg = wshOut || function($){ alert($); };
-  this.print = wshOut || function(){
-    var rx = /[&<>"]/g;
-    var map = { '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;'};
+  this.echo = wshOut || function(){
+    var rx = /\r\n|[\n\r&<>"]/g;
+    var map = { '\r\n': "<br/>", '\n': "<br/>", '\r': "<br/>", '&': "&amp;", '<': "&lt;", '>': "&gt;", '"': "&quot;" };
     function escape(m, x, h){ return map[m]; }
-    return function($){ document.writeln($.replace(rx, escape)); };
+    return function($){
+      document.writeln(arguments.length ?
+       any2string($).replace(rx, escape) + "<br/>"
+       : "<br/>");
+    };
   }();
 });
-
-print('<&br">');
